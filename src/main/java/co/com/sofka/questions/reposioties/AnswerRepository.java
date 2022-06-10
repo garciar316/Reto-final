f0a18de7835a2961bf3401548f5c3adb028484f4
garciar316 <sergio980316@hotmail.com>
2022-06-10 12:30:36 -0500

    ADD: se agrega documentación con Swagger los end points

--- src/main/java/co/com/sofka/questions/reposioties/AnswerRepository.java
+++ src/main/java/co/com/sofka/questions/reposioties/AnswerRepository.java
@@ -1,6 +1,7 @@
 package co.com.sofka.questions.reposioties;
 
 import co.com.sofka.questions.collections.Answer;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -8,7 +9,7 @@ import reactor.core.publisher.Mono;
 
 
 @Repository
-public interface AnswerRepository extends ReactiveCrudRepository<Answer, String> {
+public interface AnswerRepository extends ReactiveMongoRepository<Answer, String> {
     Flux<Answer> findAllByQuestionId(String id);
 
     Mono<Void> deleteByQuestionId(String questionId);
--- src/main/java/co/com/sofka/questions/routers/QuestionRouter.java
+++ src/main/java/co/com/sofka/questions/routers/QuestionRouter.java
@@ -3,9 +3,18 @@ package co.com.sofka.questions.routers;
 import co.com.sofka.questions.model.AnswerDTO;
 import co.com.sofka.questions.model.QuestionDTO;
 import co.com.sofka.questions.usecases.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -20,6 +29,13 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class QuestionRouter {
 
     @Bean
+    @RouterOperation(path = "/getAll", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = ListUseCase.class, method = RequestMethod.GET, beanMethod = "get",
+            operation = @Operation(operationId = "get", responses = {
+                    @ApiResponse(responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = QuestionDTO.class)))}
+            ))
     public RouterFunction<ServerResponse> getAll(ListUseCase listUseCase) {
         return route(GET("/getAll"),
                 request -> ServerResponse.ok()
@@ -29,6 +45,14 @@ public class QuestionRouter {
     }
 
     @Bean
+    @RouterOperation(path = "/getOwnerAll/{userId}", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = OwnerListUseCase.class, method = RequestMethod.GET, beanMethod = "apply",
+            operation = @Operation(operationId = "get", responses = {
+                    @ApiResponse(responseCode = "200", description = "successful operation",
+                            content = @Content(schema = @Schema(implementation = QuestionDTO.class)))},
+                    parameters = {@Parameter(in = ParameterIn.PATH, name = "userId")}
+            ))
     public RouterFunction<ServerResponse> getOwnerAll(OwnerListUseCase ownerListUseCase) {
         return route(
                 GET("/getOwnerAll/{userId}"),
@@ -42,6 +66,12 @@ public class QuestionRouter {
     }
 
     @Bean
+    @RouterOperation(path = "/create", produces = {
+            MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.POST, beanClass = CreateUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "apply", responses = {
+                    @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = QuestionDTO.class)))}
+                    , requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = QuestionDTO.class)))
+            ))
     public RouterFunction<ServerResponse> create(CreateUseCase createUseCase) {
         Function<QuestionDTO, Mono<ServerResponse>> executor = questionDTO ->  createUseCase.apply(questionDTO)
                 .flatMap(result -> ServerResponse.ok()
@@ -55,6 +85,14 @@ public class QuestionRouter {
     }
 
     @Bean
+    @RouterOperation(path = "/getOwnerAll/{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = OwnerListUseCase.class, method = RequestMethod.GET, beanMethod = "apply",
+            operation = @Operation(operationId = "apply", responses = {
+                    @ApiResponse(responseCode = "200", description = "successful operation",
+                            content = @Content(schema = @Schema(implementation = QuestionDTO.class)))},
+                    parameters = {@Parameter(in = ParameterIn.PATH, name = "id")}
+            ))
     public RouterFunction<ServerResponse> get(GetUseCase getUseCase) {
         return route(
                 GET("/get/{id}").and(accept(MediaType.APPLICATION_JSON)),
@@ -68,6 +106,12 @@ public class QuestionRouter {
     }
 
     @Bean
+    @RouterOperation(path = "/add", produces = {
+            MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.POST, beanClass = AddAnswerUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "apply", responses = {
+                    @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = QuestionDTO.class)))}
+                    , requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = AnswerDTO.class)))
+            ))
     public RouterFunction<ServerResponse> addAnswer(AddAnswerUseCase addAnswerUseCase) {
         return route(POST("/add").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(AnswerDTO.class)
@@ -80,6 +124,13 @@ public class QuestionRouter {
     }
 
     @Bean
+    @RouterOperation(path = "/delete/{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.DELETE, beanClass = DeleteUseCase.class, beanMethod = "apply"
+            , operation = @Operation(operationId = "apply", responses = {
+            @ApiResponse(responseCode = "200", description = "successful operation")},
+            parameters = {
+            @Parameter(in = ParameterIn.PATH, name = "id")}
+    ))
     public RouterFunction<ServerResponse> delete(DeleteUseCase deleteUseCase) {
         return route(
                 DELETE("/delete/{id}").and(accept(MediaType.APPLICATION_JSON)),
